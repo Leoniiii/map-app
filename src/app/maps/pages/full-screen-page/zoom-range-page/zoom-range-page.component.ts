97b175f9f9919eef46c932801bb812dbaf83e36c
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2023-12-24 10:06:32 +0100

    Add controllers

--- src/app/maps/pages/full-screen-page/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/full-screen-page/zoom-range-page/zoom-range-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import mapboxgl from 'mapbox-gl';
-(mapboxgl as any).accessToken = 'pk.eyJ1IjoibGVvbmk2IiwiYSI6ImNscGltcHlqcjAxN3MybXFvMXhyaTFvMWoifQ.DXL_D2ICY3Xk1ElIj2oARQ';
+import { Map, accessToken } from 'mapbox-gl';
+
 @Component({
   selector: 'app-zoom-range-page',
   standalone: true,
@@ -11,6 +11,8 @@ import mapboxgl from 'mapbox-gl';
   styleUrl: './zoom-range-page.component.scss'
 })
 export class ZoomRangePageComponent implements AfterViewInit {
+  public zoom = 10
+  public map?: Map;
 
   @ViewChild('map') divMap?: ElementRef
 
@@ -18,11 +20,39 @@ export class ZoomRangePageComponent implements AfterViewInit {
 
     if (!this.divMap) throw 'El elemento html no fue encontrado'
 
-    const map = new mapboxgl.Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
+      accessToken: 'pk.eyJ1IjoibGVvbmk2IiwiYSI6ImNscGltcHlqcjAxN3MybXFvMXhyaTFvMWoifQ.DXL_D2ICY3Xk1ElIj2oARQ',
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+    this.mapListener()
+  }
+
+  mapListener() {
+
+    if (!this.map) throw 'Mapa no inicializado'
+    this.map.on('zoom', () => {
+      this.zoom = this.map!.getZoom()
+    })
+
+    this.map.on('zoom', () => {
+      if (this.map!.getZoom() < 18) return
+      this.map!.zoomTo(18)
+    })
+  }
+
+  zoomIn() {
+    this.map?.zoomIn()
+  }
+
+  zoomOut() {
+    this.map?.zoomOut()
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = +value;
+    this.map?.zoomTo(this.zoom)
   }
 }
