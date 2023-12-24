a2fcfa51df2739183eecc940239a82a9fd12b9d2
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2023-12-24 09:10:00 +0100

    Add mapbox

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import mapboxgl from 'mapbox-gl';
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoibGVvbmk2IiwiYSI6ImNscGltcHlqcjAxN3MybXFvMXhyaTFvMWoifQ.DXL_D2ICY3Xk1ElIj2oARQ';
 @Component({
   selector: 'app-full-screen-page',
   standalone: true,
@@ -8,6 +10,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.scss'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw 'El elemento html no fue encontrado'
+
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 
 }
--- src/app/maps/pages/full-screen-page/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/full-screen-page/zoom-range-page/zoom-range-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import mapboxgl from 'mapbox-gl';
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoibGVvbmk2IiwiYSI6ImNscGltcHlqcjAxN3MybXFvMXhyaTFvMWoifQ.DXL_D2ICY3Xk1ElIj2oARQ';
 @Component({
   selector: 'app-zoom-range-page',
   standalone: true,
@@ -8,6 +10,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.scss'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
 
+  @ViewChild('map') divMap?: ElementRef
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw 'El elemento html no fue encontrado'
+
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
