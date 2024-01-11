c9265444ae976bcfab2bacbd9569f91264d75322
Leonides Muguercia <leonimuguercia5@gmail.com>
2024-01-11 17:47:28 +0100

    Show properties

--- src/app/maps/components/minimap/minimap.component.ts
+++ src/app/maps/components/minimap/minimap.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import mapboxgl, { Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-minimap',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './minimap.component.html',
-  styleUrl: './minimap.component.scss'
+  styleUrl: './minimap.component.scss',
 })
-export class MinimapComponent {
+export class MinimapComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+  @Input() lngLat?: [number, number];
 
+  ngAfterViewInit() {
+    if (!this.divMap?.nativeElement) throw 'Map Div not found';
+    if (!this.lngLat) throw "LngLat can't be null";
+
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 16,
+      interactive: true,
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
 }
